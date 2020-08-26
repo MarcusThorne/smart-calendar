c02cdfeb0f11cfd3b82ad26160babb549ec6da3f
zeemai <zee.hao.mai@gmail.com>
2020-08-26 15:50:13 +0100

    cal-time

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,8 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { findLocation1, findLocation2 } from '../components/findLocation'
+import { calTime } from '../components/calTime'
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +33,7 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  findLocation1();
+  findLocation2();
+  calTime();
 });
