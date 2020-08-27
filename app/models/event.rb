4ed95e06faa0804d75f3c9bdb872c95bdbad888a
rohitsharma <rohit.sharma1986@live.com>
2020-08-27 16:15:24 +0100

    errormessage

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :user
   validates :time_difference, presence: true
+  # validate :cannot_have_conflict
+  validates :start_time, :end_time, :overlap => {:scope => ["user_id", "end_date"]}
+
+  # def cannot_have_conflict
+  #   conflicts = Event.where(user: user).where("\
+  #     (start_time < :start_time AND end_time > :start_time)\
+  #     OR (start_time < :end_time AND end_time > :end_time)",
+  #     start_time: start_time, end_time: end_time)
+  #   if conflicts.present?
+  #     errors.add(:start_time, "Not possible Fool, check the time lol")
+  #   end
+  # end
 end
