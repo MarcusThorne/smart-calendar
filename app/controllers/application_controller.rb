f68fa2c7a5fc2323d3ecdc90da66766efe3d369f
Marcus Thorne <marcus_thorne475@hotmail.com>
2020-08-28 14:29:34 +0100

    redirect to daily page upon sign up/ sing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def default_url_options
   { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || daily_path
+  end
 end
