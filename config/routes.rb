d1ad83ca30698b4dc1215c7d32c93e9d348f764f
Marcus Thorne <marcus_thorne475@hotmail.com>
2020-08-28 11:19:11 +0100

    added a new home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'events#daily'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :calendars, only: [:index] do
   resources :events
   # end
   get "/weekly", to: "events#weekly"
   get "/monthly", to: "events#monthly"
+  get "/daily", to: "events#daily"
 
   require "sidekiq/web"
     mount Sidekiq::Web => '/sidekiq'
