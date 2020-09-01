1c143ba73ca85f32ee204cb9f569b7a636014c40
Aidan Robinson <aidanfrederick123@gmail.com>
2020-09-01 10:44:42 +0100

    sorted the time zone as well as sorting sidekiq when you delete an event

--- app/jobs/event_job.rb
+++ app/jobs/event_job.rb
@@ -2,7 +2,10 @@ class EventJob < ApplicationJob
   queue_as :default
 
   def perform(event_id)
+    # This job only happens when the job is about to begin not when the job is created
     event = Event.find(event_id.to_i)
-    UserMailer.with(user: event.user).event.deliver_later
+    UserMailer.with(user: event.user).event.deliver_later if event
+    # the above if statement searches whether the event id is present at the time of the
+    # job and if its not there then cancels the job even though the job appears in sidekiq.
   end
 end
