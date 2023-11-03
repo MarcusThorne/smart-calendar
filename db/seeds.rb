4ab04229e798963a6b8e00276cd7236772e930d0
Marcus Thorne <marcus_thorne475@hotmail.com>
2023-11-03 16:11:26 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Users.create(email: 'admin@admin.com', encrypted_password: '123456', first_name: 'admin', last_name: 'admin')
